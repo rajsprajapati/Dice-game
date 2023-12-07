e9efc7d6e273232acbf1f029fe73a43248519f95
Raj-s-prajapati <115444348+rajsprajapati@users.noreply.github.com>
2023-12-07 21:50:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function clickme() {
+    var randomNumber1 = "./images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", randomNumber1);
+
+    var randomNumber2 = "./images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomNumber2);
+}
+
