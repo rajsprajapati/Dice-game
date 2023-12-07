559d041de9b972a0a0c79fe51b596aa7680c31d1
Raj-s-prajapati <115444348+rajsprajapati@users.noreply.github.com>
2023-12-07 22:15:43 +0530

    Update index.js
    h1 change

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ function clickme() {
 
     var randomNumber2 = "dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", randomNumber2);
+
+    if(randomNumber1>randomNumber2){
+        document.querySelector("h1").textContent="Palyer 1 wins!";
+    }
+    else if(randomNumber2>randomNumber1){
+        document.querySelector("h1").textContent="Palyer 2 wins!";
+    }
+    else{
+        document.querySelector("h1").textContent="Draw!";
+    }
 }
 
