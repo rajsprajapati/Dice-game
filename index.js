e28f0650fe833e615214883f6eaa0abc49c97324
Raj-s-prajapati <115444348+rajsprajapati@users.noreply.github.com>
2023-12-07 21:56:59 +0530

    Update index.js
    dice img

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function clickme() {
-    var randomNumber1 = "./dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
+    var randomNumber1 = "dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", randomNumber1);
 
-    var randomNumber2 = "./dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
+    var randomNumber2 = "dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", randomNumber2);
 }
 
